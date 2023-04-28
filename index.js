6b52a4cba2ab1e09b2f586055caf9e6312a352fc
Ammar Mostafa <87477414+amaralanany12345@users.noreply.github.com>
2023-04-28 22:15:49 +0200

    final typescript code

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+let textInput = document.querySelector(".textInput");
+let addTask = document.querySelector(".addTask");
+let taskContainer = document.getElementById("taskContainer");
+let Tasks = [];
+addTask.addEventListener("click", function () {
+    let newTask = {
+        task: textInput.value
+    };
+    Tasks.push(newTask);
+    showTask();
+});
+function showTask() {
+    let task = ``;
+    for (let i = 0; i < Tasks.length; i++) {
+        task += `
+        <h2 class="task">${Tasks[i].task}</h2>
+        <button class="deleteTask" onclick=deleteTask(${i})>X</button>
+        `;
+    }
+    taskContainer.innerHTML = task;
+}
+function deleteTask(i) {
+    Tasks.splice(i, 1);
+    showTask();
+}
---
+++ index.ts
@@ -0,0 +1,27 @@
+let textInput = document.querySelector(".textInput") as HTMLInputElement
+let addTask = document.querySelector(".addTask") as HTMLButtonElement
+let taskContainer:any=document.getElementById("taskContainer")
+let Tasks:any=[]
+addTask.addEventListener("click",function(){
+    let newTask={
+        task:textInput.value
+    }
+    Tasks.push(newTask)
+    showTask()
+})
+
+function showTask(){
+    let task=``
+    for (let i=0;i<Tasks.length;i++){
+        task+=`
+        <h2 class="task">${Tasks[i].task}</h2>
+        <button class="deleteTask" onclick=deleteTask(${i})>X</button>
+        `
+    }
+    taskContainer.innerHTML=task
+}
+function deleteTask(i:number){
+    Tasks.splice(i,1)
+    showTask()
+
+}
